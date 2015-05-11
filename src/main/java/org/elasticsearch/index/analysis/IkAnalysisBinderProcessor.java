7d0da0edc1dc66289ecc61e2d31a2bcb3ae86a1f
Yanlong Wang <avastms@ghostunix.org>
2015-05-11 18:11:55 +0800

    go

--- src/main/java/org/elasticsearch/index/analysis/IkAnalysisBinderProcessor.java
+++ src/main/java/org/elasticsearch/index/analysis/IkAnalysisBinderProcessor.java
@@ -10,13 +10,13 @@ public class IkAnalysisBinderProcessor extends AnalysisModule.AnalysisBinderProc
 
     @Override public void processAnalyzers(AnalyzersBindings analyzersBindings) {
         analyzersBindings.processAnalyzer("ik", IkAnalyzerProvider.class);
-        super.processAnalyzers(analyzersBindings);
+        //super.processAnalyzers(analyzersBindings);
     }
 
 
     @Override
     public void processTokenizers(TokenizersBindings tokenizersBindings) {
       tokenizersBindings.processTokenizer("ik", IkTokenizerFactory.class);
-      super.processTokenizers(tokenizersBindings);
+      //super.processTokenizers(tokenizersBindings);
     }
 }
--- src/main/java/org/elasticsearch/plugin/analysis/ik/AnalysisIkPlugin.java
+++ src/main/java/org/elasticsearch/plugin/analysis/ik/AnalysisIkPlugin.java
@@ -6,7 +6,13 @@ import org.elasticsearch.index.analysis.IkAnalysisBinderProcessor;
 import org.elasticsearch.plugins.AbstractPlugin;
 
 
+import org.elasticsearch.common.logging.ESLogger;
+import org.elasticsearch.common.logging.Loggers;
+
+
 public class AnalysisIkPlugin extends AbstractPlugin {
+
+    public static ESLogger logger=Loggers.getLogger("ik-analyzer");
 
     @Override public String name() {
         return "analysis-ik";
@@ -19,9 +25,16 @@ public class AnalysisIkPlugin extends AbstractPlugin {
 
 
     @Override public void processModule(Module module) {
+        logger.info('really in processModule');
         if (module instanceof AnalysisModule) {
+            logger.info('really in addProcessor');
             AnalysisModule analysisModule = (AnalysisModule) module;
             analysisModule.addProcessor(new IkAnalysisBinderProcessor());
         }
+    }
+    
+    public void onModule(AnalysisModule module) {
+        logger.info('really in onModule');
+        module.addProcessor(new IkAnalysisBinderProcessor());
     }
 }
